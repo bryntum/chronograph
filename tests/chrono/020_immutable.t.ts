c61ba0e4b1cbe18652e0967873805a2e7b98a79f
Nickolay <nickolay8@gmail.com>
2018-12-05 17:44:57 +0300

    use similar api for both Immutable and MutableBox in tests

--- tests/chrono/020_immutable.t.ts
+++ tests/chrono/020_immutable.t.ts
@@ -9,7 +9,7 @@ StartTest(t => {
 
         t.is(node.hasValue(), false, 'No value initially provided')
         t.is(node.get(), undefined, 'No value initially provided')
-        t.is(node.previous, undefined, 'Can track the old value')
+        t.is(node.getPrevious(), undefined, 'Can track the old value')
     })
 
 
@@ -18,7 +18,7 @@ StartTest(t => {
 
         t.is(node.hasValue(), true, 'Can set value')
         t.is(node.get(), 1, 'Can set value')
-        t.is(node.previous, undefined, 'Can track the old value')
+        t.is(node.getPrevious(), undefined, 'Can track the old value')
     })
 
 
@@ -31,23 +31,23 @@ StartTest(t => {
 
         t.is(node1.hasValue(), true, 'Can set value')
         t.is(node1.get(), 1, 'Can set value')
-        t.is(node1.previous, undefined, 'Can track the old value')
+        t.is(node1.getPrevious(), undefined, 'Can track the old value')
 
         const node2     = node1.set(2)
 
         t.isnt(node2, node1, 'Non empty node derives a new one on `set`')
 
         t.is(node2.get(), 2, 'Can update value')
-        t.is(node2.previous.get(), 1, 'Can track the old value')
+        t.is(node2.getPrevious().get(), 1, 'Can track the old value')
 
         const node3     = node2.set(3)
 
         t.isnt(node3, node2, 'Non empty node derives a new one on `set`')
 
         t.is(node3.get(), 3, 'Can update value')
-        t.is(node3.previous.get(), 2, 'Can track the old value')
-        t.is(node3.previous.previous.get(), 1, 'Can track the old value')
-        t.is(node3.previous.previous.previous, undefined, 'Can track the old value')
+        t.is(node3.getPrevious().get(), 2, 'Can track the old value')
+        t.is(node3.getPrevious().getPrevious().get(), 1, 'Can track the old value')
+        t.is(node3.getPrevious().getPrevious().getPrevious(), undefined, 'Can track the old value')
     })
 
 
@@ -61,13 +61,13 @@ StartTest(t => {
         t.isnt(branch2, node, 'New branch derived')
 
         t.isnt(branch1, branch2, 'New branch derived')
-        t.is(branch1.previous, branch2.previous, 'Both reference the same previous atom')
+        t.is(branch1.getPrevious(), branch2.getPrevious(), 'Both reference the same previous atom')
 
         t.is(branch1.get(), 2, 'Can update value')
-        t.is(branch1.previous.get(), 1, 'Can track the old value')
+        t.is(branch1.getPrevious().get(), 1, 'Can track the old value')
 
         t.is(branch2.get(), 3, 'Can update value')
-        t.is(branch2.previous.get(), 1, 'Can track the old value')
+        t.is(branch2.getPrevious().get(), 1, 'Can track the old value')
     })
 
 
