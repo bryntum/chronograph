27356fe42aa0e534dcadf140f568a0acf7a264de
Nickolay <nickolay8@gmail.com>
2018-12-07 16:41:27 +0300

    initial test for minimal necessary mutations

--- tests/graph/030_graph.t.ts
+++ tests/graph/030_graph.t.ts
@@ -116,4 +116,73 @@ StartTest(t => {
     })
 
 
+    t.iit('2 mutations in graph context', t => {
+        const graph : GraphBox  = MinimalGraphBox.new({ id : 'graph' })
+
+        const box1 : Box        = graph.getBox('inp1')
+        const box2 : Box        = graph.getBox('inp2')
+        const box1p2 : Box      = graph.getBox('1p2')
+        const box3 : Box        = graph.getBox('inp3')
+        const res : Box         = graph.getBox('res')
+
+        const mutation1 = MinimalChronoMutationNode.new({
+            id          : 'mutation1',
+
+            input       : [ box1, box2 ],
+            output      : [ box1p2 ],
+
+            calculation : (v1, v2) => {
+                return v1 + v2
+            }
+        })
+
+        const mutation2 = MinimalChronoMutationNode.new({
+            id          : 'mutation2',
+
+            input       : [ box1p2, box3 ],
+            output      : [ res ],
+
+            calculation : (v1, v2) => {
+                return v1 + v2
+            }
+        })
+
+        const calculation1Spy   = t.spyOn(mutation1, 'calculation')
+        const calculation2Spy   = t.spyOn(mutation2, 'calculation')
+
+        graph.addMutation(mutation1)
+        graph.addMutation(mutation2)
+
+        box1.set(0)
+        box2.set(0)
+        box3.set(1)
+
+        graph.propagate()
+
+        t.expect(calculation1Spy).toHaveBeenCalled(1)
+        t.expect(calculation2Spy).toHaveBeenCalled(1)
+
+        t.is(box1p2.get(), 0, "Correct result calculated")
+        t.is(res.get(), 1, "Correct result calculated")
+
+        // should only recalculate mutation2
+        box3.set(2)
+
+        graph.addMutation(mutation1)
+        graph.addMutation(mutation2)
+
+        const calculation1Spy$  = t.spyOn(mutation1, 'calculation')
+        const calculation2Spy$  = t.spyOn(mutation2, 'calculation')
+
+        graph.propagate()
+
+        t.expect(calculation1Spy$).toHaveBeenCalled(0)
+        t.expect(calculation2Spy$).toHaveBeenCalled(1)
+
+        t.is(res.get(), 2, "Correct result calculated")
+        t.is(res.getPrevious().get(), 1, "Can track old value")
+    })
+
+
+
 })
