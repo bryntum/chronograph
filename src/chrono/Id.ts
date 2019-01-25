a996f7c44ff9558d6b1eed66a25dbaf35cbb5aa4
MaximGB <max.bazhenov@gmail.com>
2019-01-25 18:57:45 +0300

    chronoId() function cleanup

--- src/chrono/Id.ts
+++ src/chrono/Id.ts
@@ -3,28 +3,4 @@ export type ChronoId        = string | number
 
 let ATOM_ID : number = 1
 
-export const chronoId = () : ChronoId => 'chrono' + <ChronoId>(ATOM_ID++ as unknown)
-
-
-// //---------------------------------------------------------------------------------------------------------------------
-// export type ChronoNamespace = string
-//
-// export function namespacedId (ns : ChronoNamespace, id : ChronoId) : ChronoId
-// export function namespacedId (ns : ChronoNamespace) : ChronoId
-// export function namespacedId (...args : any[]) : ChronoId {
-//     const [ ns, id ] = args
-//
-//     if (args.length === 1) {
-//
-//         return `/${ns}/${chronoId()}`
-//
-//     } else if (args.length === 2) {
-//
-//         return `/${ns}/${id}`
-//
-//     } else
-//         throw new Error("Invalid call signature")
-// }
-//
-
-
+export const chronoId = () : ChronoId => `chrono${ATOM_ID++}`
