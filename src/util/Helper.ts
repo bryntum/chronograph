cd883c62e17322316391fc6484ade6e54160dbc1
Nickolay <nickolay8@gmail.com>
2019-02-18 11:57:35 +0300

    more compact

--- src/util/Helper.ts
+++ src/util/Helper.ts
@@ -1,6 +1,4 @@
-export const uppercaseFirst = (str : string) : string => {
-    return str.slice(0, 1).toUpperCase() + str.slice(1)
-}
+export const uppercaseFirst = (str : string) : string => str.slice(0, 1).toUpperCase() + str.slice(1)
 
 
 export const isAtomicValue = (value : any) : boolean => Object(value) !== value
