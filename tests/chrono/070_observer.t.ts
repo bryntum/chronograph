6e7a69819b873b830c4229ea970cdea71858f84d
Nickolay <nickolay8@gmail.com>
2019-05-17 16:18:13 +0300

    test for observer

---
+++ tests/chrono/070_observer.t.ts
@@ -0,0 +1,43 @@
+import { ChronoAtom, MinimalChronoAtom } from "../../src/chrono/Atom.js"
+import { EffectResolutionResult } from "../../src/chrono/Effect.js"
+import { ChronoGraph, MinimalChronoGraph } from "../../src/chrono/Graph.js"
+
+declare const StartTest : any
+
+StartTest(t => {
+
+    t.it('Observer should work', async t => {
+        const graph : ChronoGraph   = MinimalChronoGraph.new()
+
+        const atom1 : ChronoAtom    = graph.addNode(MinimalChronoAtom.new({ value : 0 }))
+        const atom2 : ChronoAtom    = graph.addNode(MinimalChronoAtom.new({ value : 1 }))
+
+        const atom3 : ChronoAtom    = graph.addNode(MinimalChronoAtom.new({
+            calculation     : function * (proposedValue : number) {
+                return (yield atom1) + (yield atom2)
+            }
+        }))
+
+        await graph.propagate()
+
+        const log = []
+
+        const observer  = graph.observe(function* () {
+            const value3    = yield atom3
+
+            log.push(value3)
+        })
+
+        t.isDeeply(log, [ 1 ], "Correct observation history")
+
+        await atom1.set(1)
+
+        t.isDeeply(log, [ 1, 2 ], "Correct observation history")
+
+        observer.remove()
+
+        await atom1.set(2)
+
+        t.isDeeply(log, [ 1, 2 ], "Correct observation history")
+    })
+})
