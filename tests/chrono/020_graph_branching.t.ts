c181c739a43ef00c2a74cea1974a88f2f154b9af
Nickolay <nickolay8@gmail.com>
2019-05-31 20:08:38 +0300

    test for branch isolation

--- tests/chrono/020_graph_branching.t.ts
+++ tests/chrono/020_graph_branching.t.ts
@@ -26,60 +26,43 @@ StartTest(t => {
     })
 
 
-    t.it('Cross-branch unchanged trees elimination', async t => {
-        const graph : ChronoGraph       = MinimalChronoGraph.new()
+    t.iit('Cross-branch unchanged trees elimination', async t => {
+        const graph1 : ChronoGraph       = MinimalChronoGraph.new()
 
-        const i1        = graph.variableId('i1', 0)
-        const i2        = graph.variableId('i2', 10)
+        const i1            = graph1.variableId('i1', 0)
+        const i2            = graph1.variableId('i2', 1)
+        const dispatcher    = graph1.variableId('dispatcher', i1)
 
-        const c1        = graph.identifierId('c1', function* () {
-            return (yield i1) + (yield i2)
+        const c1            = graph1.identifierId('c1', function* () {
+            return (yield (yield dispatcher)) + 1
         })
 
-        const c2        = graph.identifierId('c2', function* () {
-            return (yield i1) + (yield c1)
-        })
+        graph1.propagate()
 
-        const c3        = graph.identifierId('c3', function* () {
-            return (yield c1)
-        })
+        const graph2    = graph1.branch()
 
-        const c4        = graph.identifierId('c4', function* () {
-            return (yield c3)
-        })
+        graph2.write(dispatcher, i2)
 
-        const c5        = graph.identifierId('c5', function* () {
-            return (yield c3)
-        })
+        const c1Spy         = t.spyOn(c1, 'calculation')
 
-        const c6        = graph.identifierId('c6', function* () {
-            return (yield c5) + (yield i2)
-        })
+        graph2.propagate()
+
+        t.expect(c1Spy).toHaveBeenCalled(1)
+
+        t.isDeeply([ i1, i2, dispatcher, c1 ].map(node => graph1.read(node)), [ 0, 1, i1, 1 ], "Correct result calculated")
+        t.isDeeply([ i1, i2, dispatcher, c1 ].map(node => graph2.read(node)), [ 0, 1, i2, 2 ], "Correct result calculated")
 
         // ----------------
-        const nodes             = [ i1, i2, c1, c2, c3, c4, c5, c6 ]
+        c1Spy.reset()
 
-        const spies             = nodes.map(calculation => t.spyOn(calculation, 'calculation'))
+        graph1.write(i1, 10)
 
-        graph.propagate()
+        graph1.propagate()
 
-        t.isDeeply(nodes.map(node => graph.read(node)), [ 0, 10, 10, 10, 10, 10, 10, 20 ], "Correct result calculated")
+        t.expect(c1Spy).toHaveBeenCalled(0)
 
-        spies.forEach(spy => t.expect(spy).toHaveBeenCalled(1))
-
-        // ----------------
-        spies.forEach(spy => spy.reset())
-
-        graph.write(i1, 5)
-        graph.write(i2, 5)
-
-        graph.propagate()
-
-        t.isDeeply(nodes.map(node => graph.read(node)), [ 5, 5, 10, 15, 10, 10, 10, 15 ], "Correct result calculated")
-
-        const expectedCalls     = [ 1, 1, 1, 1, 0, 0, 0, 1 ]
-
-        spies.forEach((spy, index) => t.expect(spy).toHaveBeenCalled(expectedCalls[ index ]))
+        t.isDeeply([ i1, i2, dispatcher, c1 ].map(node => graph1.read(node)), [ 10, 1, i1, 11 ], "Correct result calculated")
+        t.isDeeply([ i1, i2, dispatcher, c1 ].map(node => graph2.read(node)), [ 0, 1, i2, 2 ], "Correct result calculated")
     })
 
 })
