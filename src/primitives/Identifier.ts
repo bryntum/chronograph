22c74f26474cbccf1bda80d9c52cdad8e8f3822c
Nickolay <nickolay8@gmail.com>
2019-05-31 14:59:25 +0300

    extracted reference to separate file

--- src/primitives/Identifier.ts
+++ src/primitives/Identifier.ts
@@ -34,38 +34,3 @@ export class Variable extends Identifier {
     }
 }
 
-
-//---------------------------------------------------------------------------------------------------------------------
-export class Reference extends Identifier {
-    ArgsT               : [ this[ 'ResultT' ], boolean ]
-
-    YieldT              : ReturnType<typeof dereference>
-
-    * calculation (value : this[ 'ResultT' ], isDereference : boolean) : CalculationIterator<this[ 'ResultT' ], this[ 'YieldT' ]> {
-        if (isDereference) {
-            return yield dereference(value)
-        } else
-            return value
-    }
-}
-
-
-//---------------------------------------------------------------------------------------------------------------------
-export class ReferenceBucket extends Identifier {
-    ArgsT               : [ this[ 'ResultT' ], boolean ]
-
-    YieldT              : ReturnType<typeof dereference>
-
-    * calculation (value : this[ 'ResultT' ], isDereference : boolean) : CalculationIterator<this[ 'ResultT' ], this[ 'YieldT' ]> {
-        if (isDereference) {
-            return yield dereference(value)
-        } else
-            return value
-    }
-}
-
-
-
-const DereferenceSymbol = Symbol("DereferenceSymbol")
-
-const dereference = (value : any) => { return { DereferenceSymbol, value } }
