9b7ad6aefb1010c6fc2b06af46d1e8aa1df4b0e4
Nickolay <nickolay8@gmail.com>
2019-05-31 20:07:47 +0300

    style and const

--- src/class/InstanceOf.ts
+++ src/class/InstanceOf.ts
@@ -3,21 +3,22 @@ import { AnyConstructor, AnyFunction } from "./Mixin.js"
 //---------------------------------------------------------------------------------------------------------------------
 export const instanceOf = <T extends AnyConstructor<object>>(base : AnyFunction<T>) : AnyFunction<T> => {
 
-    const symbol    = Symbol(base.name)
+    const symbol        = Symbol(base.name)
+
+    const isInstanceOf  = function (instance) : instance is T {
+        return Boolean(instance && instance[ symbol ])
+    }
 
     return new Proxy(base, {
         get : function (target, property, receiver) {
-
             if (property === Symbol.hasInstance)
-                return function (instance) : instance is T {
-                    return Boolean(instance && instance[symbol])
-                }
+                return isInstanceOf
             else
                 return target[ property ]
         },
 
-        apply : function (target, thisArg, argumentsList) {
-            const extendedClass = target.call(thisArg, ...argumentsList)
+        apply : function (target, context, args) {
+            const extendedClass = target.call(context, ...args)
 
             extendedClass.prototype[ symbol ] = true
 
