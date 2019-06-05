44705713ad6a29a1b3e3b7521707953d45e2cfe3
Nickolay <nickolay8@gmail.com>
2019-06-05 19:38:29 +0300

    plain performance version for inspiration

--- tests/benchmark/020_performance.bench.ts
+++ tests/benchmark/020_performance.bench.ts
@@ -27,11 +27,7 @@ StartTest(t => {
                         yield boxes[3]
                     ]
 
-                    const res   = input.reduce((sum, op) => sum + op, 0)
-
-                    // console.log(`Ident ${i}: `, res)
-
-                    return res
+                    return input.reduce((sum, op) => sum + op, 0)
                 }, i))
             }
             else if (i % 2 == 0) {
@@ -75,7 +71,7 @@ StartTest(t => {
         t.chain(
             async next => {
                 graph.write(boxes[ 0 ], 0)
-                graph.write(boxes[ 1 ], 2)
+                // graph.write(boxes[ 1 ], 2) // only few atoms changes
 
                 console.time("Calc #1")
                 // console.profile('Propagate #1')
@@ -87,20 +83,121 @@ StartTest(t => {
 
                 console.log("Result #1: ", graph.read(boxes[ boxes.length - 1 ]))
             },
-            // async next => {
-            //     graph.write(boxes[ 1 ], 0)
-            //
-            //     console.time("Calc #2")
-            //     // console.profile('Propagate #2')
-            //
-            //     graph.propagate()
-            //     // graph.propagateSync()
-            //
-            //     // console.profileEnd()
-            //     console.timeEnd("Calc #2")
-            //
-            //     console.log("Result #2: ", graph.read(boxes[ boxes.length - 1 ]))
-            // }
+            async next => {
+                console.time("Undo")
+                console.profile('Undo')
+
+                graph.undo()
+
+                console.profileEnd()
+                console.timeEnd("Undo")
+
+                console.log("Result after undo: ", graph.read(boxes[ boxes.length - 1 ]))
+            }
         )
     })
+
+
+    t.xit("Plain js version", async t => {
+        let atomNum     = 300000
+
+        let boxes       = []
+
+        console.time("Build graph")
+
+        for (let i = 0; i < atomNum; i++) {
+            if (i <= 3) {
+                boxes.push({ value : 1 })
+            }
+            else if (i <= 10) {
+                boxes.push({
+                    index       : i,
+
+                    calculation : function () {
+                        const input = [
+                            boxes[0].value,
+                            boxes[1].value,
+                            boxes[2].value,
+                            boxes[3].value,
+                        ]
+
+                        this.value = input.reduce((sum, op) => sum + op, 0)
+                    }
+                })
+            }
+            else if (i % 2 == 0) {
+                boxes.push({
+                    index       : i,
+
+                    calculation : function () {
+                        const input = [
+                            boxes[this.index - 1].value,
+                            boxes[this.index - 2].value,
+                            boxes[this.index - 3].value,
+                            boxes[this.index - 4].value
+                        ]
+
+                        this.value = input.reduce((sum, op) => (sum + op) % 10000, 0)
+                    }
+                })
+            } else {
+                boxes.push({
+                    index       : i,
+
+                    calculation : function () {
+                        const input = [
+                            boxes[this.index - 1].value,
+                            boxes[this.index - 2].value,
+                            boxes[this.index - 3].value,
+                            boxes[this.index - 4].value
+                        ]
+
+                        this.value = input.reduce((sum, op) => (sum - op) % 10000, 0)
+                    }
+                })
+            }
+        }
+
+        // console.profileEnd()
+        console.timeEnd("Build graph")
+
+        console.time("Calc #0")
+        // console.profile('Propagate #0')
+
+        boxes.forEach(atom => atom.calculation ? atom.calculation() : null)
+
+        // console.profileEnd()
+        console.timeEnd("Calc #0")
+
+        console.log("Result #0: ", boxes[ boxes.length - 1 ].value)
+
+        // t.chain(
+        //     async next => {
+        //         graph.write(boxes[ 0 ], 0)
+        //         // graph.write(boxes[ 1 ], 2)
+        //
+        //         console.time("Calc #1")
+        //         // console.profile('Propagate #1')
+        //
+        //         graph.propagate()
+        //
+        //         // console.profileEnd()
+        //         console.timeEnd("Calc #1")
+        //
+        //         console.log("Result #1: ", graph.read(boxes[ boxes.length - 1 ]))
+        //     },
+        //     async next => {
+        //         console.time("Undo")
+        //         console.profile('Undo')
+        //
+        //         graph.undo()
+        //
+        //         console.profileEnd()
+        //         console.timeEnd("Undo")
+        //
+        //         console.log("Result after undo: ", graph.read(boxes[ boxes.length - 1 ]))
+        //     }
+        // )
+    })
+
 })
