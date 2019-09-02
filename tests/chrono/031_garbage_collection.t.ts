020ffd5c2940268ca1b308fa9ae5943eb077a91a
nickolay <nickolay8@gmail.com>
2019-09-02 12:56:51 +0300

    test for garbage collection bug

--- tests/chrono/031_garbage_collection.t.ts
+++ tests/chrono/031_garbage_collection.t.ts
@@ -1,5 +1,8 @@
 import { ChronoGraph, MinimalChronoGraph } from "../../src/chrono/Graph.js"
+import { CalculatedValueGen, ImpureCalculatedValueGen } from "../../src/chrono/Identifier.js"
 import { MinimalQuark } from "../../src/chrono/Quark.js"
+import { ProposedOrCurrentValue } from "../../src/chrono/Transaction.js"
+import { CalculationIterator } from "../../src/primitives/Calculation.js"
 
 declare const StartTest : any
 
@@ -34,4 +37,48 @@ StartTest(t => {
 
         t.is(graph.baseRevision.previous, null, "No extra revisions")
     })
+
+
+    t.it('Garbage collecting should keep data dependencies', async t => {
+        const graph : ChronoGraph   = MinimalChronoGraph.new()
+
+        const var0      = graph.variableId('var0', 1)
+
+        const var1      = graph.variableId('var1', 100)
+
+        const var2      = graph.addIdentifier(CalculatedValueGen.new({
+            * calculation () : CalculationIterator<number> {
+                const value : number         = yield var1
+
+                return value + 1
+            }
+        }))
+
+        //------------------
+        graph.propagate()
+
+        // create a revision with `var1 -> var2` edge
+        t.is(graph.read(var2), 101, 'Correct value')
+
+        // now we create couple of throw-away revisions, which will garbage collect the revision with `var1 -> var2` edge
+
+        //------------------
+        graph.write(var0, 2)
+
+        graph.propagate()
+
+        //------------------
+        graph.write(var0, 3)
+
+        graph.propagate()
+
+        // and now making sure the dependency is still alive
+        //------------------
+        graph.write(var1, 10)
+
+        graph.propagate()
+
+        t.is(graph.read(var2), 11, 'Correct value')
+    })
+
 })
