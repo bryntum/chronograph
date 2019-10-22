b7378403492dfadd73db4a9dbc02a72a0dab8193
nickolay <nickolay8@gmail.com>
2019-10-22 19:51:58 +0300

    cleanup

--- src/replica/Entity.ts
+++ src/replica/Entity.ts
@@ -44,26 +44,6 @@ export const Entity = instanceOf(<T extends AnyConstructor<object>>(base : T) =>
                 $[ name ]   = this.createFieldIdentifier(field)
             })
 
-            // this.$entity.forEachField((field, name) => {
-            //     if (field.listeners) {
-            //         let listeners     = $[ name ].listeners
-            //
-            //         if (!listeners) listeners = $[ name ].listeners = new Set()
-            //
-            //         field.listeners.forEach(name => listeners.add($[ name ]))
-            //     }
-            //
-            //     if (field.listens) {
-            //         field.listens.forEach(name => {
-            //             let listeners     = $[ name ].listeners
-            //
-            //             if (!listeners) listeners = $[ name ].listeners = new Set()
-            //
-            //             listeners.add($[ field.name ])
-            //         })
-            //     }
-            // })
-
             // debugging aid
             const proxy = new Proxy($, {
                 get (target : object, property : string | number | symbol, receiver : any) : any {
