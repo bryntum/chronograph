3b2accbc19198d0378fcf7e7b12f23b0cec74ae4
nickolay <nickolay8@gmail.com>
2019-10-24 12:41:54 +0300

    reactive rendering adaptations

--- src/replica/Entity.ts
+++ src/replica/Entity.ts
@@ -90,7 +90,7 @@ export const Entity = instanceOf(<T extends AnyConstructor<object>>(base : T) =>
                 field               : field,
                 self                : this,
                 context             : this,
-                lazy                : isInstanceOf(field, ReferenceField) || isInstanceOf(field, ReferenceBucketField) ? false : true
+                // lazy                : isInstanceOf(field, ReferenceField) || isInstanceOf(field, ReferenceBucketField) ? false : true
             }
 
             //------------------
