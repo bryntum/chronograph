c3293c3e25022ceabbf360b4d78d3c1a93f8e580
nickolay <nickolay8@gmail.com>
2019-12-11 22:01:49 +0300

    - `split`, `toMap` for iterators

--- src/collection/Iterator.ts
+++ src/collection/Iterator.ts
@@ -1,4 +1,55 @@
 //---------------------------------------------------------------------------------------------------------------------
+export function split<Element> (iterable : Iterable<Element>) : [ Iterable<Element>, Iterable<Element> ] {
+    const gen1Pending : Element[]  = []
+    const gen2Pending : Element[]  = []
+
+    let iterator : Iterator<Element>
+
+    const gen1 = function * () : Generator<Element, any, undefined> {
+        if (!iterator) iterator = iterable[Symbol.iterator]()
+
+        while (true) {
+            if (gen1Pending.length) {
+                yield* gen1Pending
+                gen1Pending.length      = 0
+            }
+
+            const { value, done }       = iterator.next()
+
+            if (done) { iterator = null; break }
+
+            gen2Pending.push(value)
+
+            yield value
+        }
+    }
+
+    const gen2 = function * () : Generator<Element, any, undefined> {
+        if (!iterator) iterator = iterable[Symbol.iterator]()
+
+        while (true) {
+            if (gen2Pending.length) {
+                yield* gen2Pending
+                gen2Pending.length      = 0
+            }
+
+            if (!iterator) break
+
+            const { value, done }       = iterator.next()
+
+            if (done) { iterator = null; break }
+
+            gen1Pending.push(value)
+
+            yield value
+        }
+    }
+
+    return [ gen1(), gen2() ]
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------
 export function* inBatchesBySize<Element> (iterator : Iterable<Element>, batchSize : number) : Iterable<Element[]> {
     if (batchSize < 0) throw new Error("Batch size needs to a natural number")
     batchSize   = batchSize | 0
@@ -68,7 +119,7 @@ export function reduce<Element, Result> (iterator : Iterable<Element>, func : (a
     let acc : Result        = initialAcc
 
     for (const el of iterator) {
-        acc                 = func(acc, el, i)
+        acc                 = func(acc, el, i++)
     }
 
     return acc
@@ -173,6 +224,15 @@ export class ChainedIteratorClass<T> {
     }
 
 
+    split () : ChainedIteratorClass<T> {
+        const [ iter1, iter2 ] = split(this.iterator)
+
+        this.iterator   = iter2
+
+        return CI(iter1)
+    }
+
+
     inBatchesBySize (batchSize : number) : ChainedIteratorClass<T[]> {
         return new ChainedIteratorClass(inBatchesBySize(this.iterator, batchSize))
     }
@@ -194,7 +254,9 @@ export class ChainedIteratorClass<T> {
 
 
     concat<K> () : T extends Iterable<K> ? ChainedIteratorClass<K> : never {
-        return new ChainedIteratorClass(concatIterable<K>(this.iterator as any)) as any
+        return new ChainedIteratorClass<K>(
+            concatIterable<K>(this.iterator as (T extends Iterable<K> ? Iterable<T> : never))
+        ) as (T extends Iterable<K> ? ChainedIteratorClass<K> : never)
     }
 
 
@@ -223,7 +285,7 @@ export class ChainedIteratorClass<T> {
     }
 
 
-    * [Symbol.iterator] () {
+    * [Symbol.iterator] () : IterableIterator<T> {
         yield* this.iterator
     }
 
@@ -238,15 +300,14 @@ export class ChainedIteratorClass<T> {
     }
 
 
-    // toMap () : T extends [ infer K, infer V ] ? Map<K, V> : never  {
-    //     return new Map(this as (T extends [ infer K, infer V ] ? Iterable<[ K, V ]> : never))
-    // }
+    toMap<K, V> () : T extends [ K, V ] ? Map<K, V> : never  {
+        return new Map<K, V>(this.iterator as (T extends [ K, V ] ? Iterable<T> : never)) as (T extends [ K, V ] ? Map<K, V> : never)
+    }
 
 
     flush () {
-        for (const element of this) {}
+        for (const element of this.iterator) {}
     }
-
 }
 
 export const ChainedIterator = <T>(iterator : Iterable<T>) : ChainedIteratorClass<T> => new ChainedIteratorClass<T>(iterator)
