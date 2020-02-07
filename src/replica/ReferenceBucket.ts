8a628100d6947e5cba72c2a238e2eecece04090b
nickolay <nickolay8@gmail.com>
2020-02-07 14:08:30 +0300

    test fixed

--- src/replica/ReferenceBucket.ts
+++ src/replica/ReferenceBucket.ts
@@ -25,19 +25,29 @@ export const bucket : FieldDecorator<typeof ReferenceBucketField> =
     (fieldConfig?, fieldCls = ReferenceBucketField) => generic_field(fieldConfig, fieldCls)
 
 
+export enum BucketMutationType {
+    'Add'       = 'Add',
+    'Remove'    = 'Remove'
+}
+
+export type BucketMutation  = {
+    type        : BucketMutationType,
+    entity      : Entity
+}
+
 //---------------------------------------------------------------------------------------------------------------------
 export class ReferenceBucketQuark extends Mixin(
     [ Quark ],
     (base : ClassUnion<typeof Quark>) =>
 
 class ReferenceBucketQuark extends base {
-    oldRefs             : Set<Entity>   = undefined
-    newRefs             : Set<Entity>   = undefined
+    mutations           : BucketMutation[]  = []
+
     previousValue       : Set<Entity>   = undefined
 
 
     hasProposedValueInner () : boolean {
-        return Boolean(this.oldRefs || this.newRefs)
+        return this.mutations.length > 0
     }
 }){}
 
@@ -62,9 +72,7 @@ export class ReferenceBucketIdentifier extends Mixin(
         addToBucket (transaction : Transaction, entity : Entity) {
             const quark         = transaction.getWriteTarget(this) as ReferenceBucketQuark
 
-            if (!quark.newRefs) quark.newRefs = new Set()
-
-            quark.newRefs.add(entity)
+            quark.mutations.push({ type : BucketMutationType.Add, entity })
 
             const baseRevision  = transaction.baseRevision
 
@@ -75,9 +83,7 @@ export class ReferenceBucketIdentifier extends Mixin(
         removeFromBucket (transaction : Transaction, entity : Entity) {
             const quark         = transaction.getWriteTarget(this) as ReferenceBucketQuark
 
-            if (!quark.oldRefs) quark.oldRefs = new Set()
-
-            quark.oldRefs.add(entity)
+            quark.mutations.push({ type : BucketMutationType.Remove, entity })
 
             const baseRevision  = transaction.baseRevision
 
@@ -89,10 +95,16 @@ export class ReferenceBucketIdentifier extends Mixin(
             const quark                         = quarkArg as ReferenceBucketQuark
             const newValue : Set<Entity>        = new Set(quark.previousValue)
 
-            // need to remove the old references first and then add new - to allow re-adding just removed reference
-            if (quark.oldRefs) quark.oldRefs.forEach(entity => newValue.delete(entity))
+            for (let i = 0; i < quark.mutations.length; i++) {
+                const { type, entity } = quark.mutations[ i ]
 
-            if (quark.newRefs) quark.newRefs.forEach(entity => newValue.add(entity))
+                if (type === BucketMutationType.Remove) {
+                    newValue.delete(entity)
+                }
+                else if (type === BucketMutationType.Add) {
+                    newValue.add(entity)
+                }
+            }
 
             return newValue
         }
