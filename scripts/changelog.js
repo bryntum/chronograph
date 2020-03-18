47defc67d34681dbfe7f92cf11a4dac4cf669372
nickolay <nickolay8@gmail.com>
2020-03-18 17:57:31 +0300

    progress on release

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,11 +1,12 @@
 const fs    = require('fs')
 const path  = require('path')
 
+console.log("YOYOYO", process.cwd())
+
+
 const now           = Date.now()
 const version       = require('./package.json').version
 const versionStr    = `${version} ${now.getFullYear()}-${ prependZero(now.getMonth() + 1, 2) }-${ prependZero(now.getDay(), 2) } ${ prependZero(now.getHour(), 2) }:${ prependZero(now.getMinutes(), 2) }`
-
-console.log("YOYOYO", process.cwd())
 
 const prependZero = (int, minLength) => {
     const str   = String(int)
