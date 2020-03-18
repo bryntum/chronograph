ebe9895fa881241e92d0dd342001bae459cf78c9
nickolay <nickolay8@gmail.com>
2020-03-18 18:04:37 +0300

    progress on release

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,15 +1,15 @@
 const fs    = require('fs')
 const path  = require('path')
 
-const now           = new Date()
-const version       = require('../package.json').version
-const versionStr    = `${version} ${now.getFullYear()}-${ prependZero(now.getMonth() + 1, 2) }-${ prependZero(now.getDate(), 2) } ${ prependZero(now.getHours(), 2) }:${ prependZero(now.getMinutes(), 2) }`
-
 const prependZero = (int, minLength) => {
     const str   = String(int)
 
     return '0000000'.slice(0, minLength - str.length) + str
 }
+
+const now           = new Date()
+const version       = require('../package.json').version
+const versionStr    = `${version} ${now.getFullYear()}-${ prependZero(now.getMonth() + 1, 2) }-${ prependZero(now.getDate(), 2) } ${ prependZero(now.getHours(), 2) }:${ prependZero(now.getMinutes(), 2) }`
 
 const updateVersion = () => {
     let changelog = fs.readFileSync('CHANGELOG.md', 'utf8')
