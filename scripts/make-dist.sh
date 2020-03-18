80ce69c889746061dde9eacd0ae3873e481080c0
nickolay <nickolay8@gmail.com>
2020-03-18 13:39:28 +0300

    progress on release

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# exit if any of command has failed
+set -e
+
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+node "$DIR/has_changes.js"
 
 DIST="$DIR/../dist"
 
@@ -8,5 +13,12 @@ rm -rf "$DIST"
 
 mkdir -p "$DIST"
 
-rsync -r "$DIR/.." "$DIST" \
-  --exclude "scripts" --exclude ".git" --exclude ".idea" --exclude "dist" --exclude "benchmarks"
+git worktree add "$DIST" --no-checkout --detach
+
+(
+cd "$DIST"
+git checkout HEAD
+)
+#
+#rsync -r "$DIR/.." "$DIST" \
+#    --exclude "scripts" --exclude ".git" --exclude ".idea" --exclude "dist" --exclude "benchmarks" --exclude "node_modules"
