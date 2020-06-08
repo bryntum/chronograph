819cd3095f0e2c5a937ed1c03b050af12e5f54b1
nickolay <nickolay8@gmail.com>
2020-06-08 11:44:58 +0300

    uniqable

--- src/util/Uniqable.ts
+++ src/util/Uniqable.ts
@@ -9,13 +9,13 @@ let UNIQABLE : number = MIN_SMI
 
 
 // in-place mutation
-export const compact = (array : Uniqable[]) => {
+export const compact = <T extends Uniqable>(array : T[]) : T[] => {
     const uniqableId : number   = ++UNIQABLE
 
     let uniqueIndex : number    = -1
 
     for (let i = 0; i < array.length; ++i) {
-        const element : Uniqable    = array[ i ]
+        const element : T       = array[ i ]
 
         if (element.uniqable !== uniqableId) {
             element.uniqable    = uniqableId
@@ -29,4 +29,32 @@ export const compact = (array : Uniqable[]) => {
     // assuming its better to not touch the array's `length` property
     // unless we really have to
     if (array.length !== uniqueIndex + 1) array.length = uniqueIndex + 1
+
+    return array
 }
+
+
+// // in-place mutation + forEach
+// export const compactAndForEach = (array : Uniqable[], func : (el : Uniqable) => any) => {
+//     const uniqableId : number   = ++UNIQABLE
+//
+//     let uniqueIndex : number    = -1
+//
+//     for (let i = 0; i < array.length; ++i) {
+//         const element : Uniqable    = array[ i ]
+//
+//         if (element.uniqable !== uniqableId) {
+//             element.uniqable    = uniqableId
+//
+//             ++uniqueIndex
+//
+//             func(element)
+//
+//             if (uniqueIndex !== i) array[ uniqueIndex ] = element
+//         }
+//     }
+//
+//     // assuming its better to not touch the array's `length` property
+//     // unless we really have to
+//     if (array.length !== uniqueIndex + 1) array.length = uniqueIndex + 1
+// }
