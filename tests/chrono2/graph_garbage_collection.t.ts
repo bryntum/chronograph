0f58195b4b0e16c8aa963fd824ac1812b21902eb
nickolay <nickolay8@gmail.com>
2020-07-07 17:19:58 +0300

    todo test

--- tests/chrono2/graph_garbage_collection.t.ts
+++ tests/chrono2/graph_garbage_collection.t.ts
@@ -97,7 +97,7 @@ StartTest(t => {
     })
 
 
-    t.it('Garbage collecting should keep data dependencies', async t => {
+    t.it('Garbage collection should keep data dependencies', async t => {
         const graph : ChronoGraph   = ChronoGraph.new({ historyLimit : 1 })
 
         const var0      = new Box(1, 'var0')
@@ -138,7 +138,7 @@ StartTest(t => {
     })
 
 
-    t.it('Garbage collecting should keep deep old data', async t => {
+    t.it('Garbage collection should keep deep old data', async t => {
         const graph1 : ChronoGraph   = ChronoGraph.new({ historyLimit : 1 })
 
         const var0      = new Box(1, 'var0')
@@ -169,7 +169,7 @@ StartTest(t => {
     })
 
 
-    t.it('Garbage collecting should not throw when working with branches', async t => {
+    t.it('Garbage collection should not throw when working with branches', async t => {
         const graph1 : ChronoGraph   = ChronoGraph.new({ historyLimit : 1 })
 
         const var1      = new Box(100, 'var1')
@@ -227,10 +227,8 @@ StartTest(t => {
     })
 
 
-    t.it('Garbage collecting should clear old data', async t => {
+    t.todo('Garbage collection should work with branches', async t => {
         const graph1 : ChronoGraph   = ChronoGraph.new({ historyLimit : 1 })
-
-        const var0      = new Box(1, 'var0')
 
         const var1      = new Box(100, 'var1')
 
@@ -239,28 +237,56 @@ StartTest(t => {
             calculation : () => var1.read() + 1
         })
 
-        const nodes         = [ var0, var1, var2 ]
+        const nodes         = [ var1, var2 ]
 
-        //------------------
         graph1.addAtoms(nodes)
 
-        t.isDeeply(nodes.map(node => node.read()), [ 1, 100, 101 ], "Correct result calculated")
-
-        graph1.commit()
+        t.isDeeply(nodes.map(node => node.read()), [ 100, 101 ], "Correct result calculated")
 
         //------------------
+        graph1.commit()
+
+        const graph2        = graph1.branch({ historyLimit : 1 })
+        const $             = node => graph2.checkout(node)
+
+        t.isDeeply(nodes.map(node => $(node).read()), [ 100, 101 ], "Correct result calculated")
+
+        //------------------
+        var1.write(20)
+        graph1.commit()
+
+        var1.write(30)
+        graph1.commit()
+
         var1.write(200)
         graph1.commit()
 
         var1.write(300)
-        t.isDeeply(nodes.map(node => node.read()), [ 1, 300, 301 ], "Correct result calculated")
-
-        //------------------
         graph1.commit()
 
-        t.is(var1.immutable.previous.previous.previous, ZeroBoxImmutable)
+        t.isDeeply(nodes.map(node => node.read()), [ 300, 301 ], "Correct result calculated")
+        t.isDeeply(nodes.map(node => $(node).read()), [ 100, 101 ], "Correct result calculated")
+
+        t.is(graph1.currentTransaction.previous.previous, undefined, "No extra revisions")
+        t.is(var1.immutable.previous.previous.previous, ZeroBoxImmutable, "No extra revisions")
+
+        //------------------
+        $(var1).write(2)
+        graph2.commit()
+
+        $(var1).write(3)
+        graph2.commit()
+
+        $(var1).write(20)
+        graph2.commit()
+
+        $(var1).write(30)
+        graph2.commit()
+
+        t.isDeeply(nodes.map(node => node.read()), [ 300, 301 ], "Correct result calculated")
+        t.isDeeply(nodes.map(node => $(node).read()), [ 30, 31 ], "Correct result calculated")
+
+        t.is(graph2.currentTransaction.previous.previous, undefined, "No extra revisions")
+        t.is($(var1).immutable.previous.previous.previous, ZeroBoxImmutable, "No extra revisions")
     })
-
-
-
 })
