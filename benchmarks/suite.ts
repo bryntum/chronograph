e2141b925e55421be576471d74296151ace9c228
nickolay <nickolay8@gmail.com>
2020-07-13 14:51:51 +0300

    progress on benchmarks

--- benchmarks/suite.ts
+++ benchmarks/suite.ts
@@ -1,10 +1,10 @@
 import { runAllGraphPopulation } from "./allocation.js"
 import { runAllDeepChanges } from "./deep_changes.js"
-import { runAllMassive } from "./chrono/graphless/deep_changes2.js"
+// import { runAllMassive } from "./chrono/graphless/deep_changes2.js"
 import { runAllShallowChanges } from "./shallow_changes.js"
 
 export const runAll = async () => {
-    await runAllMassive()
+    // await runAllMassive()
     await runAllDeepChanges()
     await runAllShallowChanges()
     await runAllGraphPopulation()
