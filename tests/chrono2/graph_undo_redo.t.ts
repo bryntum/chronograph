5fb5515602a6403b51756a308ba37ea57a93060a
nickolay <nickolay8@gmail.com>
2020-07-16 20:07:00 +0300

    test for redo

--- tests/chrono2/graph_undo_redo.t.ts
+++ tests/chrono2/graph_undo_redo.t.ts
@@ -83,6 +83,7 @@ StartTest(t => {
         const box1     = new Box(0)
 
         const box2     = new CalculableBox({
+            lazy        : false,
             calculation : () => box1.read()  + 1
         })
 
@@ -107,12 +108,57 @@ StartTest(t => {
         t.is(box1.read(), 0, 'Correct value #3')
         t.is(box2.read(), 1, 'Correct value #3')
 
-        // //--------------
-        // graph.redo()
-        //
-        // t.is(box1.read(), 10, 'Correct value #4')
-        // t.is(box2.read(), 11, 'Correct value #4')
+        //--------------
+        graph.redo()
+
+        t.is(box1.read(), 10, 'Correct value #4')
+        t.is(box2.read(), 11, 'Correct value #4')
     })
+
+
+    t.xit('Undo/redo of the box value - lazy atoms read', async t => {
+        const graph : ChronoGraph   = ChronoGraph.new({ historyLimit : 2 })
+
+        const box1     = new Box(0)
+
+        const box2     = new CalculableBox({
+            lazy        : false,
+            calculation : () => box1.read()  + 1
+        })
+
+        graph.addAtoms([ box1, box2 ])
+
+        graph.commit()
+
+        t.is(box1.read(), 0, 'Correct value #1')
+        t.is(box2.read(), 1, 'Correct value #1')
+
+        //--------------
+        box1.write(10)
+
+        graph.commit()
+
+        t.is(box1.read(), 10, 'Correct value #2')
+        t.is(box2.read(), 11, 'Correct value #2')
+
+        //--------------
+        graph.undo()
+
+        t.is(box1.read(), 0, 'Correct value #3')
+        t.is(box2.read(), 1, 'Correct value #3')
+
+        // TODO
+        // the following redo does not work - because reading from the box2 right above
+        // creates a new iteration and clears the undo/redo queue
+        // need to figure out how to deal with this (or leave as is??)
+
+        //--------------
+        graph.redo()
+
+        t.is(box1.read(), 10, 'Correct value #4')
+        t.is(box2.read(), 11, 'Correct value #4')
+    })
+
 
 
 })
