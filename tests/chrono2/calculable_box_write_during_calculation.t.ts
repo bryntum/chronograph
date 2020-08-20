abb23db4d47838cd0af9c6b24d502bfbbef227a6
nickolay <nickolay8@gmail.com>
2020-08-20 15:19:16 +0300

    - extra test for WDC

--- tests/chrono2/calculable_box_write_during_calculation.t.ts
+++ tests/chrono2/calculable_box_write_during_calculation.t.ts
@@ -1,5 +1,6 @@
 import { setCompactCounter } from "../../src/chrono2/atom/Node.js"
 import { Box } from "../../src/chrono2/data/Box.js"
+import { CalculableBox } from "../../src/chrono2/data/CalculableBox.js"
 import { ChronoGraph } from "../../src/chrono2/graph/Graph.js"
 import { GraphGen } from "../util.js"
 
@@ -54,7 +55,7 @@ StartTest(t => {
         })
 
 
-        t.iit(prefix + "Should be possible to write to another box during calculation with distant depedencies", t => {
+        t.it(prefix + "Should be possible to write to another box during calculation with distant dependencies", t => {
             const box0      = new Box(1, 'box0')
             // const box00     = new Box(0, 'box00')
 
@@ -68,12 +69,11 @@ StartTest(t => {
             const box2   = graphGen.calculableBox({
                 name        : 'box2',
                 calculation : eval(graphGen.calc(function* () {
-                    if (window.DEBUG) debugger
+                    // if (window.DEBUG) debugger
 
                     const value1 = (yield box1) + 1
 
                     if (value1 > 10) {
-                        debugger
                         box0.write(0)
                     }
 
@@ -93,7 +93,7 @@ StartTest(t => {
             spy1.reset()
             spy2.reset()
 
-            window.DEBUG = true
+            // window.DEBUG = true
 
             box0.write(20)
 
@@ -181,8 +181,70 @@ StartTest(t => {
             t.is(iden2.read(), 13, 'Correct value')
             t.is(iden3.read(), 0, 'Correct value')
         })
+
+
+        t.it(prefix + 'Should not perform extra computations on graph mutation', t => {
+            const box0      = new Box(0)
+            const box00     = new Box(0)
+
+            let count1      = 0
+            const box1 : CalculableBox<number>     = graphGen.calculableBox({
+                calculation : eval(graphGen.calc(function* () {
+                    count1++
+                    return box0.read() + 1
+                }))
+            })
+
+            let count11     = 0
+            const box11 : CalculableBox<number>    = graphGen.calculableBox({
+                calculation : eval(graphGen.calc(function* () {
+                    count11++
+                    return box00.read() + 1
+                }))
+            })
+
+            const dispatcher : Box<CalculableBox<number>> = new Box(box1)
+
+            let count2      = 0
+            const box2      = graphGen.calculableBox({
+                calculation : eval(graphGen.calc(function* () {
+                    count2++
+
+                    const box : CalculableBox<number>      = dispatcher.read()
+
+                    return box.read() + 1
+                }))
+            })
+
+            t.is(box2.read(), 2)
+            t.is(box11.read(), 1)
+
+            t.is(count1, 1)
+            t.is(count11, 1)
+            t.is(count2, 1)
+
+            //---------------
+            count1 = count11 = count2 = 0
+
+            box00.write(1)
+            // this switches the `box2` to different computation path
+            // on that path `box2` triggers the computation of `box11` which is stale at that moment
+            // once `box11` recomputes it propagates the staleness to its derivations,
+            // which, should not affect the `Calculating` state of those, because that will trigger
+            // an extra computation
+            dispatcher.write(box11)
+
+            t.is(box2.read(), 3)
+
+            t.is(count1, 0)
+            t.is(count11, 1)
+
+            // the key assertion of the test
+            t.is(count2, 1)
+        })
+
     }
 
     doTest(t, GraphGen.new({ sync : true }))
-    // doTest(t, GraphGen.new({ sync : false }))
+    doTest(t, GraphGen.new({ sync : false }))
 })
